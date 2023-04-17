c78dc3c4495de3324b502b15e6d124d561c452d8
pravin palkar <pravinpalkar7709@gmail.com>
2023-04-17 23:39:55 +0530

    git to github demo

--- src/test/java/Firest/TestNG/GitDemo.java
+++ src/test/java/Firest/TestNG/GitDemo.java
@@ -1,5 +1,27 @@
 package Firest.TestNG;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GitDemo {
+	WebDriver driver;
+	public void setup() throws IOException
+	{
+		String path=System.getProperty("user.dir");
+		String filepath=path+"\\test.properties";
+		String browserpath=path+"\\chromedriver.exe";
+		
+		FileReader fread=new FileReader(filepath);
+		Properties p=new Properties();
+		p.load(fread);
+		String value = p.getProperty("baseurl");
+		System.setProperty("wedriver.chrome.driver", browserpath);
+		 driver=new ChromeDriver();
+		driver.get(value);
+	}
 
 }
